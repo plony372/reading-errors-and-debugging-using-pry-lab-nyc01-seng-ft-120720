6fe3b8fe44d2a3a664ec8cf469ddde53c88f00c2
plony benav <menachemfuterfas@gmail.com>
2020-11-11 03:22:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-   newString = 10 * "s" + string
+   
+    10 * "s" + string
   else
-    newString
+    string
   end
-  newString
 end
